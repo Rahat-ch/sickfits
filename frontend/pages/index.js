7d7d5081bec422668afd6cbbe64357ed2593dbe5
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2019-02-27 23:37:32 -0500

    frontend boilerplate set up and back end boilerplate set

---
+++ frontend/pages/index.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+const Home = props => (
+    <div>
+        <p>AY yo bruh!</p>
+    </div>
+)
+
+export default Home;
---
+++ frontend/pages/sell.js
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+
+const Sell = props => (
+    <div>
+        <p>AY yo sellin time</p>
+    </div>
+)
+
+export default Sell;
