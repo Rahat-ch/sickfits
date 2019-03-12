f2fabb4c0c82f71bab97f7c3e7ee2634f6a08b85
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2019-03-12 11:09:35 -0400

    experimenting with mutations - all working

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,10 @@
-const Mutations = {};
+const Mutations = {
+    createDog(parent, args, ctx, info) {
+        //create a dog
+        const newDog = { name: args.name };
+        global.dogs.push(newDog);
+        return newDog;
+    }
+};
 
 module.exports = Mutations;
