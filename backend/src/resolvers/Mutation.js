71dae8b3f194f63f43907106bf0026c71b52418e
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2019-03-20 13:19:37 -0400

    backend set up for authentication

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+
 const Mutations = {
     async createItem(parent, args, ctx, info) {
         // TODO: Check if they are logged in
@@ -39,6 +42,28 @@ const Mutations = {
         // TODO
         // 3. Delete it!
         return ctx.db.mutation.deleteItem({ where }, info);
+    },
+    async signup(parent, args, ctx, info) {
+        //make their email lowercase
+        args.email = args.email.toLowerCase();
+        // hash their password
+        const password = await bcrypt.hash(args.password, 10);
+        const user = await ctx.db.mutation.createUser({
+            data: {
+                ...args,
+                password,
+                permissions: { set: ['USER'] },
+            },
+        }, info);
+        //create jwt token for them
+        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+        //set the jwt as a cookie on the response
+        ctx.response.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24* 365, // 1 year cookie
+        });
+        //return the user to the browser
+        return user;
     },
 };
 
